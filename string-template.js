f2745ecfcebe8a50bc438bd7d2c583af76a3e622
saddamhr <44530098+saddamhr@users.noreply.github.com>
2020-03-03 00:08:17 +0600

    string-template

---
+++ string-template.js
@@ -0,0 +1,15 @@
+const firstName = 'Justin';
+const lastName = 'TimberLake';
+//const fullName = firstName +' '+ lastName + ' is a good boy';
+const fullName = `${firstName} ${lastName} is a good boy.`;
+// const multiLine = 'I love you\n'
+//     + 'I miss you\n'
+//     + 'I need you';
+
+ const multiLine = `I love you
+I miss you
+I need you`;
+
+console.log(multiLine);
+
+//console.log(fullName);
