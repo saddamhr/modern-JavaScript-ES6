600d566a0e9df23833b24177309868aace7fb2d0
saddamhr <44530098+saddamhr@users.noreply.github.com>
2020-03-02 23:40:42 +0600

    let VS const

---
+++ let-const.js
@@ -0,0 +1,24 @@
+const hubby = "Elias Kanchon";
+//hubby = "Omar Sunny"
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+//numbers = ["Sunny", "Salman", "Omit Hasan"];
+const nayok = {
+    name: "Sakib Khan",
+    phone: 017
+}
+
+//console.log(nayok);
+let patientName = "Rahim";
+
+patientName = "Fatima";
+console.log(patientName);
+
+let sum = 0;
+for( let i = 0; i <= 10; i++) {
+    sum += i;
+}
+//console.log(i);
